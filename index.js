1fc5317e9fca4fec882a22b46ceeaa360c6be8e6
WinnieAtieno <winnieotieno773@gmail.com>
2024-06-25 13:33:06 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+const employee  = {
+    name:"Winnie Atieno",
+    streetAddress: "B4"
+}
+//create functions
+
+    // update object properties undestructively
+function updateEmployeeWithKeyAndValue(employee,key,value){
+   const updatedEmployee = {...employee}; //makes a copy of the original object
+   updatedEmployee[key] = value;
+   
+    return updatedEmployee
+
+}
+
+    //update onject dustructively by changing the original array
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+   employee[key] = value;
+   return employee
+}
+
+    // delete a property undestructively
+function deleteFromEmployeeByKey(employee, key){
+    const deleteKey = {...employee}
+     delete deleteKey[key]
+     return deleteKey
+}
+    // delete a property destructively
+    function destructivelyDeleteFromEmployeeByKey(employee, key){
+               delete employee[key]
+         return employee
+    }
+ 
+
+
